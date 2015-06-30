37457691ba29680e9acd192bfbb2ff852f2e0364
Zhennning.Su <329595547@qq.com>
2015-06-30 20:37:40 +0800

    already comparecard def

--- test.py
+++ test.py
@@ -10,7 +10,50 @@ def dealcard(num,alreadycard=[]):
     
     return selectcard
     
+    
+def comparecard(card,maincolor=[]):
+    if len(maincolor):
+        for i in range(len(card)):
+            if card[i][0]!="e" and card[i][0]!="w":
+                maincolor = card[i][0]
+    maxindex = -1
+            
+    for i in range(len(card)):
+        w = card[i][0].find("w")
+        if w!=-1:
+            maxindex = i
+    
+    if maxindex == -1:
+        for i in range(len(card)):
+            if card[i][0] == maincolor:
+                if maxindex == -1:
+                    maxindex = i
+                if int(card[i][1:])>int(card[maxindex][1:]):
+                    maxindex = i
+                    
+    if maxindex == -1:
+        minorcolor = "e"
+        for i in range(len(card)):
+            if card[i][0]!="e" and minorcolor =="e":
+                print "aaa"
+                minorcolor = card[i][0]
+                maxindex = i
+            if minorcolor != "e" and card[i][0] == minorcolor:
+                if int(card[i][1:])>int(card[maxindex][1:]):
+                    maxindex = i
+    
+    if maxindex == -1:
+        maxindex = 0
+                
+    return maxindex
+                
+             
+            
+            
+    
+    
+    
 if __name__ == '__main__':
-    a=["w1","c4"]
-    print dealcard(58)
+    a=["e1","e2","e3","c4"]
+    print comparecard(a,['h'])
     
